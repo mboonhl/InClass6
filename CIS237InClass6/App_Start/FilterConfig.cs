337c2d02118e3863bdf8a169facadc02966028b3
Morgan Boon <mboonhl@gmail.com>
2016-12-01 17:20:58 -0500

    Initial Commit

---
+++ CIS237InClass6/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CIS237InClass6
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CIS237InClass6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CIS237InClass6.Startup))]
+namespace CIS237InClass6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
